5acb0a4a6df6914c5242ca2e72a9781188a56cef
Ülari Ainjärv <ylari@digipurk.ee>
2023-08-14 21:19:36 +0300

    Modified

--- lib/flutter_pw_validator.dart
+++ lib/flutter_pw_validator.dart
@@ -191,7 +191,8 @@ class FlutterPwValidatorState extends State<FlutterPwValidator> {
           new Flexible(
             flex: 7,
             child: new Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisAlignment: MainAxisAlignment.start,
 
                 //Iterate through the condition map entries and generate new ValidationTextWidget for each item in Green or Red Color
                 children: _conditionsHelper.getter()!.entries.map((entry) {
