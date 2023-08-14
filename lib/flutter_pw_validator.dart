399b78800bbe04ed56aeec1edcfcb0025869ac40
Ülari Ainjärv <ylari@digipurk.ee>
2023-08-14 21:26:53 +0300

    Modified

--- lib/flutter_pw_validator.dart
+++ lib/flutter_pw_validator.dart
@@ -186,7 +186,7 @@ class FlutterPwValidatorState extends State<FlutterPwValidator> {
                 for (bool value in _conditionsHelper.getter()!.values)
                   if (value == false)
                     new ValidationBarComponent(color: widget.defaultColor),
-                SizedBox(height: 10.0),
+                SizedBox(height: 12.0),
               ],
             ),
           ),
